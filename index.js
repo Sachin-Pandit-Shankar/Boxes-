55325a501f471081e30544ca375cf4d63dc01e10
sachinpandit <sachinpandit1434gmail.com>
2023-04-15 15:53:37 +0000

    Boxes

---
+++ index.js
@@ -0,0 +1,12 @@
+const Box = (props) => {
+  //  Write your code here.
+};
+
+const element = (
+  //  Write your code here.
+  <div className="bg-container">
+    <h1 className="main-heading">Boxes</h1>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
