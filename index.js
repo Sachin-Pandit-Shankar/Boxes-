964bb6bd0ef4bab7600060c54c54c62fa7946735
Sachin pandit <127498624+Sachin-Pandit-Shankar@users.noreply.github.com>
2023-07-09 12:39:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const Box = (props) => {
   //  Write your code here.
+  const { className, Text } = props;
+  return <div className={`${className}`}>{Text}</div>;
 };
 
 const element = (
   //  Write your code here.
   <div className="bg-container">
-    <h1 className="main-heading">Boxes</h1>
+    <h1>Boxes</h1>
+    <div className="box-container">
+      <Box className="box1" Text="Small" />
+      <Box className="box2" Text="Medium" />
+      <Box className="box3" Text="Large" />
+    </div>
   </div>
 );
 
